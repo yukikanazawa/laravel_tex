25920e69a2e70998f0057762007a46dd3031e387
EC2 Default User <ec2-user@ip-172-31-19-48.ap-northeast-1.compute.internal>
2021-05-08 14:04:30 +0900

    commit8-2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,6 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
